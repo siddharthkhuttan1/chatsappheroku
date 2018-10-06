480eb91c65cb7ede77876656e1d4ac529a053eb7
Ankit Singh <iamankit2612@gmail.com>
2018-10-06 19:14:12 +0530

    Update index.js
    code size can be reduced.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * Created by anshul on 18/7/17.
+ * Created by anshul wadhawan on 18/7/17.
  */
 var express=require('express');
 var app=express();
@@ -12,27 +12,20 @@ var PORT=process.env.PORT||4000;
 
 var activeusers={};
 io.on('connection',function (socket) {
-
     console.log("Connection established");
-
     socket.on('send_message',function(data){
-        io.emit('get',data);
-        // socket.emit('get',data);
+        io.emit('get',data);     // socket.emit('get',data);
     });
-
     socket.on('user',function(data){
         activeusers[socket.id]=data;
         io.emit('activeusers',activeusers);
     })
-
     socket.on('disconnect', function(){
         delete activeusers[socket.id];
         console.log('user disconnected');
         io.emit('activeusers',activeusers);
     });
-
 })
-
 server.listen(PORT,function () {
     console.log("Server is running on port "+PORT);
 })
